de9a045d12a9e873a75382f4ce3676360dad476a
beylax <manuel.baldoni04@gmail.com>
2022-10-25 21:34:06 +0200

    Cors fix 2

--- next.config.js
+++ next.config.js
@@ -2,17 +2,26 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+};
+
+module.exports = {
 	async rewrites() {
 		return [
 			{
 				source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "false" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+				destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "false" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
 			},
 		];
 	},
